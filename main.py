8b12e1a0fc3e4e8193ddcf3e8027803b5cd975e1
Nikolai Kuzmin <7homo7sapiens7@gmail.com>
2020-05-18 22:51:00 +0300

    * улучшенная версия

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import csv
 from random import randint
 from datetime import datetime
 
-estimates_count = 1000
+sprint_tasks_size = 10
 points = [1,2,3,5,8,13,20]
 
 class Employee:
@@ -13,17 +13,53 @@ class Employee:
         self.testtask_accuracy = testtask_accuracy
 
     def get_estimate(self, expected_estimate):
-        devtask_estimate = expected_estimate.devtask_estimate + random.uniform(-self.devtasks_accuracy, self.devtasks_accuracy)
-        testtask_estimate = expected_estimate.testtask_estimate + random.uniform(-self.testtask_accuracy, self.testtask_accuracy)
-        return Estimation(devtask_estimate, testtask_estimate)
+        devtask_estimation = expected_estimate.devtask_estimation + random.uniform(-self.devtasks_accuracy, self.devtasks_accuracy)
+        testtask_estimation = expected_estimate.testtask_estimation + random.uniform(-self.testtask_accuracy, self.testtask_accuracy)
+        return Estimation(devtask_estimation, testtask_estimation)
 
 class Estimation:
-    def __init__(self, devtask_estimate, testtask_estimate):
-        self.devtask_estimate = devtask_estimate
-        self.testtask_estimate = testtask_estimate
+    def __init__(self, devtask_estimation, testtask_estimation):
+        self.devtask_estimation = devtask_estimation
+        self.testtask_estimation = testtask_estimation
+
+    def get_summ(self):
+        return self.devtask_estimation + self.testtask_estimation
+
+    def get_max(self):
+        return max(self.devtask_estimation, self.testtask_estimation)
+
+    def get_avg(self):
+        return (self.devtask_estimation + self.testtask_estimation)/2
+
+class EstimationStrategy:
+    def get_total_diff(self, expected_estimations, actual_developer_estimations, actual_tester_estimations):
+        zipped = zip(expected_estimations, actual_developer_estimations, actual_tester_estimations)
+        total_diff = sum(self.get_diff(*row) for row in zipped)
+        return abs(total_diff)
+
+class CommonEstimationStrategy(EstimationStrategy):
+    def get_diff(self, expected_estimation, actual_developer_estimation, actual_tester_estimation):
+        dev_diff = expected_estimation.get_summ() - actual_developer_estimation.get_summ()
+        test_diff = expected_estimation.get_summ() - actual_tester_estimation.get_summ()
+        return dev_diff + test_diff
+
+class SeparateGetMaxEstimationStrategy(EstimationStrategy):
+    def get_diff(self, expected_estimation, actual_developer_estimation, actual_tester_estimation):
+        expected_max = expected_estimation.get_max()
+        actual_max = max(actual_developer_estimation.devtask_estimation, actual_tester_estimation.testtask_estimation)
+        return expected_max - actual_max
+
+class SeparateGetAvgEstimationStrategy(EstimationStrategy):
+    def get_diff(self, expected_estimation, actual_developer_estimation, actual_tester_estimation):
+        expected_avg = expected_estimation.get_avg()
+        actual_avg = (actual_developer_estimation.devtask_estimation + actual_tester_estimation.testtask_estimation)/2
+        return expected_avg - actual_avg
 
 def simulate(simulations_count):
     result = []
+    common_strategy = CommonEstimationStrategy()
+    separate_max_strategy = SeparateGetMaxEstimationStrategy()
+    separate_avg_strategy = SeparateGetAvgEstimationStrategy()
     for _ in range(simulations_count):
         dev_dev_accuracy = random.uniform(0, 0.5)
         dev_test_accuracy = random.uniform(0.2, 1)
@@ -32,57 +68,24 @@ def simulate(simulations_count):
         developer = Employee(dev_dev_accuracy, dev_test_accuracy)
         tester = Employee(test_dev_accuracy, test_test_accuracy)
         accuracies = (dev_dev_accuracy, dev_test_accuracy, test_dev_accuracy, test_test_accuracy)
-        simulation = simulate_once(developer, tester)
+
+        simulation = simulate_once(developer, tester, common_strategy, separate_max_strategy, separate_avg_strategy)
         result.append(accuracies + simulation)
     return result
 
-def simulate_once(developer, tester):
+def simulate_once(developer, tester, common_strategy, separate_avg_strategy, separate_max_strategy):
     random.seed(datetime.now())
-    expected_estimates = [get_random_points() for i in range(estimates_count)]
-    dev_actual_estimates =  [developer.get_estimate(expected_estimate) for expected_estimate in expected_estimates]
-    test_actual_estimates = [tester.get_estimate(expected_estimate) for expected_estimate in expected_estimates]
+    expected_estimations = [get_random_estimations() for i in range(sprint_tasks_size)]
+    actual_developer_estimations =  [developer.get_estimate(expected_estimation) for expected_estimation in expected_estimations]
+    actual_tester_estimations = [tester.get_estimate(expected_estimation) for expected_estimation in expected_estimations]
     
-    total_common = get_total_estimate_diff_common_case(developer, tester, expected_estimates, dev_actual_estimates, test_actual_estimates)
-    total_max = get_total_estimate_diff_max_case(developer, tester, expected_estimates, dev_actual_estimates, test_actual_estimates)
-    total_avg = get_total_estimate_diff_avg_case(developer, tester, expected_estimates, dev_actual_estimates, test_actual_estimates)
+    total_common = common_strategy.get_total_diff(expected_estimations, actual_developer_estimations, actual_tester_estimations)
+    total_max = separate_avg_strategy.get_total_diff(expected_estimations, actual_developer_estimations, actual_tester_estimations)
+    total_avg = separate_max_strategy.get_total_diff(expected_estimations, actual_developer_estimations, actual_tester_estimations)    
     return (total_common, total_max, total_avg)    
 
-#когда всё вместе оценивается
-def get_total_estimate_diff_common_case(developer, tester, expected_estimates, dev_actual_estimates, test_actual_estimates):
-    dev_total = sum_all_estimates(dev_actual_estimates)
-    test_total = sum_all_estimates(test_actual_estimates)
-    actual_total = dev_total + test_total
-
-    expected_total = sum_all_estimates(expected_estimates) * 2
-    return abs(expected_total - actual_total)
-
-#по отдельности, берем максимум
-def get_total_estimate_diff_max_case(developer, tester, expected_estimates, dev_actual_estimates, test_actual_estimates):
-    dev_total = sum(estimate.devtask_estimate for estimate in dev_actual_estimates)
-    test_total = sum(estimate.testtask_estimate for estimate in test_actual_estimates)
-    actual_max = max(dev_total,test_total)
-
-    expected_dev = sum(estimate.devtask_estimate for estimate in expected_estimates)
-    expected_test = sum(estimate.testtask_estimate for estimate in expected_estimates)
-    expected_max = max(expected_dev, expected_test)
-    return abs(expected_max - actual_max)
-
-#по отдельности, берем среднее
-def get_total_estimate_diff_avg_case(developer, tester, expected_estimates, dev_actual_estimates, test_actual_estimates):
-    dev_total = sum(estimate.devtask_estimate for estimate in dev_actual_estimates)
-    test_total = sum(estimate.testtask_estimate for estimate in test_actual_estimates)
-    actual_avg = (dev_total + test_total)/2
-
-    expected_dev = sum(estimate.devtask_estimate for estimate in expected_estimates)
-    expected_test = sum(estimate.testtask_estimate for estimate in expected_estimates)
-    expected_avg = (expected_dev + expected_test)/2
-    return abs(expected_avg - actual_avg)
-
-def sum_all_estimates(estimates):
-    return sum(estimate.devtask_estimate + estimate.testtask_estimate for estimate in estimates)
-
 #получаем оценки фибоначчи
-def get_random_points():
+def get_random_estimations():
     dev_points = points[randint(0, len(points)-1)]
     test_points = points[randint(0, len(points)-1)]
     return Estimation(dev_points, test_points)
@@ -95,6 +98,6 @@ def print_simulation_result(simulation_result, filepath):
         for row in simulation_result:
             employee_writer.writerow(row)
 
-simulation_result = simulate(100)
-print_simulation_result(simulation_result, 'simulations.csv')
+simulation_result = simulate(1000)
+print_simulation_result(simulation_result, 'simulation.csv')
 
